1d3bc1659aacaf70c809368198a7f83b4e42f563
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2018-02-27 14:48:46 +0100

    Database

---
+++ monitoring/publishers/database.py
@@ -0,0 +1,61 @@
+from sqlalchemy import BigInteger, ForeignKey, Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+MonitoringBase = declarative_base()
+
+
+class Build(MonitoringBase):
+    __tablename__ = 'builds'
+
+    id = Column(Integer, primary_key=True)
+    job_name = Column(String(4096), nullable=False)
+    build_id = Column(Integer, nullable=False)
+    build_url = Column(String(4096), nullable=False)
+    finished_at_secs = Column(BigInteger, nullable=False)
+    status = Column(String(4096), nullable=False)
+    duration_millis = Column(BigInteger, nullable=False)
+
+    @classmethod
+    def from_build_stats(cls, build_stats):
+        build = cls(
+            job_name = build_stats.job_name,
+            build_id = build_stats.build_id,
+            build_url = build_stats.build_url,
+            finished_at_secs = build_stats.finished_at_secs,
+            status = build_stats.status,
+            duration_millis = build_stats.duration_millis,
+        )
+
+        build.stages = [Stage.from_stage_stats(stage) for stage in build_stats.stages]
+
+        return build
+
+    def __repr__(self):
+        return "<Build(id={}, name={}, build_id={})>".format(self.id, self.job_name, self.build_id)
+
+
+class Stage(MonitoringBase):
+    __tablename__ = 'stages'
+
+    id = Column(Integer, primary_key=True)
+    build_id = Column(Integer, ForeignKey('builds.id'))
+    name = Column(String(4096), nullable=False)
+    status = Column(String(4096), nullable=False)
+    duration_millis = Column(BigInteger, nullable=False)
+
+    build = relationship('Build', back_populates='stages')
+
+    @classmethod
+    def from_stage_stats(cls, stage_stats):
+        return cls(
+            name = stage_stats.name,
+            status = stage_stats.status,
+            duration_millis = stage_stats.duration_millis,
+        )
+
+    def __repr__(self):
+        return "<Stage(id={}, build_id={}, name={})>".format(self.id, self.build.build_id, self.name)
+
+
+Build.stages = relationship('Stage', order_by=Stage.id, back_populates='build')
---
+++ monitoring/tests/test_database.py
@@ -0,0 +1,131 @@
+import unittest
+from stats import BuildStats, StageStats
+from publishers.database import Build, Stage
+
+
+class TestObjectStringifying(unittest.TestCase):
+    def test_build_stringifying(self):
+        build = Build(job_name='Test',
+                      build_id=1234,
+                      build_url='test',
+                      finished_at_secs=5678,
+                      status='SUCCESS',
+                      duration_millis=4321)
+
+        self.assertEqual(str(build), '<Build(id=None, name=Test, build_id=1234)>')
+
+    def test_stage_stringifying(self):
+        build = Build(job_name='Test',
+                      build_id=1234,
+                      build_url='test',
+                      finished_at_secs=5678,
+                      status='SUCCESS',
+                      duration_millis=4321)
+
+        stage = Stage(name='TestStage',
+                      status='SUCCESS',
+                      duration_millis=1010)
+
+        build.stages.append(stage)
+
+        self.assertEqual(str(stage), '<Stage(id=None, build_id=1234, name=TestStage)>')
+
+
+class TestObjectConversions(unittest.TestCase):
+    def test_stage_from_stage_stats(self):
+        stage_stats = StageStats(
+            name = 'Stage1',
+            status = 'OK',
+            duration_millis = 123
+        )
+
+        stage = Stage.from_stage_stats(stage_stats)
+
+        self.assertIsNotNone(stage)
+        self.assertEqual(stage.id, None)
+        self.assertEqual(stage.build_id, None)
+        self.assertEqual(stage.name, 'Stage1')
+        self.assertEqual(stage.status, 'OK')
+        self.assertEqual(stage.duration_millis, 123)
+        self.assertEqual(stage.build, None)
+
+    def test_build_from_build_stats(self):
+        build_stats = BuildStats(
+            job_name = 'MyJob',
+            build_url = 'http://localhost/',
+            build_id = 10,
+            finished_at_secs = 123,
+            status = 'SUCCESS',
+            duration_millis = 3,
+            stages = [],
+        )
+
+        build = Build.from_build_stats(build_stats)
+
+        self.assertIsNotNone(build)
+        self.assertEqual(build.id, None)
+        self.assertEqual(build.job_name, 'MyJob')
+        self.assertEqual(build.build_id, 10)
+        self.assertEqual(build.build_url, 'http://localhost/')
+        self.assertEqual(build.finished_at_secs, 123)
+        self.assertEqual(build.duration_millis, 3)
+        self.assertEqual(build.status, 'SUCCESS')
+        self.assertIsNotNone(build.stages)
+        self.assertEqual(len(build.stages), 0)
+
+    def test_build_from_build_and_stages_stats(self):
+        build_stats = BuildStats(
+            job_name = 'MyJob',
+            build_url = 'http://localhost/',
+            build_id = 10,
+            finished_at_secs = 123,
+            status = 'SUCCESS',
+            duration_millis = 3,
+            stages = [
+                StageStats(
+                    name = 'Stage1',
+                    status = 'OK',
+                    duration_millis = 123
+                ),
+                StageStats(
+                    name = 'Stage2',
+                    status = 'FAIL',
+                    duration_millis = 321
+                ),
+            ],
+        )
+
+        build = Build.from_build_stats(build_stats)
+
+        self.assertIsNotNone(build)
+        self.assertEqual(build.id, None)
+        self.assertEqual(build.job_name, 'MyJob')
+        self.assertEqual(build.build_id, 10)
+        self.assertEqual(build.build_url, 'http://localhost/')
+        self.assertEqual(build.finished_at_secs, 123)
+        self.assertEqual(build.duration_millis, 3)
+        self.assertEqual(build.status, 'SUCCESS')
+        self.assertIsNotNone(build.stages)
+        self.assertEqual(len(build.stages), 2)
+
+        self.assertIsNotNone(build.stages[0])
+        self.assertEqual(build.stages[0].id, None)
+        self.assertEqual(build.stages[0].build_id, None)
+        self.assertEqual(build.stages[0].name, 'Stage1')
+        self.assertEqual(build.stages[0].status, 'OK')
+        self.assertEqual(build.stages[0].duration_millis, 123)
+        self.assertEqual(build.stages[0].build, build)
+
+        self.assertIsNotNone(build.stages[1])
+        self.assertEqual(build.stages[1].id, None)
+        self.assertEqual(build.stages[1].build_id, None)
+        self.assertEqual(build.stages[1].name, 'Stage2')
+        self.assertEqual(build.stages[1].status, 'FAIL')
+        self.assertEqual(build.stages[1].duration_millis, 321)
+        self.assertEqual(build.stages[1].build, build)
+
+    def test_invalid_build_stats(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
