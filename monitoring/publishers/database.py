0dfa9f1623686a0b35ff949655e6967aa565ea76
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2018-02-28 12:54:25 +0100

    Cleanup

--- monitoring/publishers/database.py
+++ monitoring/publishers/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import BigInteger, ForeignKey, Column, DateTime, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
 MonitoringBase = declarative_base()
 
 
